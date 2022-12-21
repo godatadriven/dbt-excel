d886de702b181745bd9bbaf26355608f8eaada13
Henk Griffioen <hgrif@users.noreply.github.com>
2022-12-21 10:12:03 +0100

    Fix function definition

--- dbt/adapters/duckdb/relation.py
+++ dbt/adapters/duckdb/relation.py
@@ -16,9 +16,7 @@ class DuckDBRelation(BaseRelation):
     external_location: Optional[str] = None
 
     @classmethod
-    def create_from_source(
-        cls: Type[Self], source: ParsedSourceDefinition, **kwargs: Any
-    ) -> Self:
+    def create_from_source(cls: Type[Self], source: ParsedSourceDefinition, **kwargs: Any) -> Self:
 
         # Some special handling here to allow sources that are external files to be specified
         # via a `external_location` meta field. If the source's meta field is used, we include
