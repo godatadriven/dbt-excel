e346121be7cd8a6e220b0b730f0406ff49c5b909
juanperafan@mdmail.nl <juanperafan@mdmail.nl>
2023-02-28 14:38:50 +0100

    Add first macros. They still only work on BigQuery. But it should be easy to translate them

---
+++ dbt_project/macros/remove_partial_duplicates.sql
@@ -0,0 +1,35 @@
+{#
+Remove partial duplicates using a ranking function.
+Needless to say, you will arbitrarly lose rows.
+It filters out any row besides the first one  per column.
+
+Example:
+
+    Input: `size`
+
+    | size | color |
+    |------+-------|
+    | S    | red   |
+    | S    | blue  |
+    | S    | red   |
+    | M    | red   |
+
+    select *
+    from public.test
+    {{ remove_partial_duplicates('size') }}
+    
+    Output:
+
+    | size | color | 
+    |------+-------+
+    | S    | red   | 
+    | M    | red   |
+
+Arguments:
+    column: Column name, required
+#}
+
+{% macro remove_partial_duplicates(column_name) %}
+qualify row_number() over (partition by {{ column_name }}) = 1
+{% endmacro %}
+
