05fe1560644dbb0770e80ba01deac0ec41daa079
Cor Zuurmond <jczuurmond@protonmail.com>
2023-03-24 22:31:54 +0100

    Remove macro splitpart

--- dbt/include/excel/macros/utils/splitpart.sql
+++
@@ -1,9 +0,0 @@
-{% macro excel__split_part(string_text, delimiter_text, part_number) %}
-
-  {% if part_number >= 0 %}
-    coalesce(string_split({{ string_text }}, {{ delimiter_text }})[ {{ part_number }} ], '')
-  {% else %}
-    {{ dbt._split_part_negative(string_text, delimiter_text, part_number) }}
-  {% endif %}
-
-{% endmacro %}
